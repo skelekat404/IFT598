8ab8bee8017b302cfdc86ed174eeff25ef0118f3
skelekat404 <morgancivida@gmail.com>
2024-03-24 16:29:54 -0700

    Add logger middleware

--- app.js
+++ app.js
@@ -15,5 +15,5 @@ app.get('/', (req, res) => {
 // Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port 3000`);
+  console.log(`Server is running on port 3000 `);
 });
